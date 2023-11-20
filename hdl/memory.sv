7f5ae8c0faeb3b60e9bf51a3ca29b03a8a2f2ae3
Laker Newhouse <lnewhouse@cfs.energy>
2023-11-20 15:39:37 -0500

    Refactor memory.sv
    Still likely there are bugs.

--- hdl/memory.sv
+++ hdl/memory.sv
@@ -10,7 +10,6 @@ module memory #(
     parameter ADDR_LENGTH = $clog2(375),  // 96 bits in a line. 36kb/96 = 375
     parameter INSTRUCTION_WIDTH = 32      // number of bits per instruction
 ) (
-    // OP codes: 4'b1000 is line addr, 4'b1001 is load immediate, 4'b1010 is load from buffer, 4'b1100 is write to buffer
     // Use first 4-bit reg for loading immediate. 4'b0 means loading 0th word in the line, 4'b1 means 1st, ... , 4'b101 means 5th 
     // We have FMA_COUNT * 3 = 6 words per line right now
     // ADDR_LENGTH needs only 9 bits for now (since we are reading entire lines), which lives in the immediate section of the instr
@@ -18,19 +17,44 @@ module memory #(
     input wire rst_in,
     input wire [LINE_WIDTH - 1 : 0] buffer_read_in,
     // We assume that the buffer will keep the valid flag high until memory reads the content and go idle
-    input wire [INSTRUCTION_WIDTH - 1 : 0] instr_in,
+    input wire [0 : INSTRUCTION_WIDTH - 1] instr_in,
     input wire instr_valid_in,
     output logic idle_out, // 1 when idle, 0 when busy
     output logic [LINE_WIDTH - 1 : 0] abc_out,  // for each FMA i, abc_in[i] is laid out as "a b c" 
     output logic abc_valid_out,
 );
 
+
+    enum logic[3:0] {
+        // ------------------------------------------------------------------------
+        // | 4 bit op code | 4 bit reg | 16 bit immediate | 4 bit reg | 4 bit reg |
+        // ------------------------------------------------------------------------
+        OP_NOP     = 4'b0000,  // no op
+        OP_END     = 4'b0001,  // end execution 
+        OP_XOR     = 4'b0010,  // xor(a_reg, b_reg):
+                               //    Places bitwise xor in a_reg
+        OP_ADDI    = 4'b0011,  // addi(a_reg, b_reg, val):
+                               //    Places sum of b_reg and val in a_reg
+        OP_BGE     = 4'b0100,  // bge(a_reg, b_reg):
+                               //    Sets compare_reg to 1 iff a_reg >= b_reg
+        OP_JUMP    = 4'b0101   // jump(jump_to):
+                               //    Jumps to instruction at immediate index jump_to, if compare_reg is 1.
+        OP_SMA     = 4'b0110   // sma(val):
+                               //    Set memory address to the immediate val in the data cache.
+        OP_LOADI   = 4'b0111   // loadi(a_reg, val):
+                               //    Load immediate val into line at memory address, at word a_reg (not value at a_reg, but the direct bits).
+        OP_LOADB   = 4'b1000   // loadb(val):
+                               //    Load FMA buffer contents into the immediate addr in the data cache.
+        OP_WRITEB  = 4'b1001   // writeb(val):
+                               //    Write contents of immediate addr in the data cache to FMA blocks. 
+    } isa;
+
     // accumulate FMA_COUNT * 3 = 6 words per line and read / write stuff into the BRAM in lines of 6 words each
 
     logic [ADDR_LENGTH - 1 : 0] addr;
     logic [LINE_WIDTH - 1 : 0] bram_temp_in;
     logic [LINE_WIDTH - 1 : 0] bram_in;
-    logic [FMA_COUNT * 3] bram_valid_in;
+    logic [FMA_COUNT * 3 - 1 : 0] bram_valid_in;
     logic [LINE_WIDTH - 1 : 0] bram_out;
     logic load_imm_error;
     logic op_code_error;
@@ -41,12 +65,12 @@ module memory #(
 
     always_ff @(posedge clk_in) begin
         if (rst_in) begin
+            idle_out <= 1;
             addr <= 0;
             bram_temp_in <= 0;
             bram_in <= 0;
             bram_valid_in <= 0;
             bram_out <= 0;
-            idle_out <= 1;
             abc_out <= 0;
             abc_valid_out <= 0;
             load_imm_error <= 0;
@@ -55,72 +79,60 @@ module memory #(
             write_flag <= 0;
             bram_read <= 0;
             bram_write <= 0;
-        end
-        else if (instr_valid_in) begin
-            case (instr_in[31:28])
-                4'b1000: begin
-                    addr <= instr_in[23:8];
-                    idle_out <= 0;
-                    bram_read <= 0;
-                    bram_write <= 0;
-                end
-                4'b1001: begin
-                    case (instr_in[27:24]) 
-                        4'b0: begin
-                            bram_temp_in[LINE_WIDTH - 1 : LINE_WIDTH - 1 * WORD_WIDTH] <= instr_in[23:8];
-                            bram_valid_in[5] <= 1;
-                        end
-                        4'b1: begin
-                            bram_temp_in[LINE_WIDTH - 1 - 1 * WORD_WIDTH : LINE_WIDTH - 2 * WORD_WIDTH] <= instr_in[23:8];
-                            bram_valid_in[4] <= 1;
-                        end
-                        4'b10: begin
-                            bram_temp_in[LINE_WIDTH - 1 - 2 * WORD_WIDTH : LINE_WIDTH - 3 * WORD_WIDTH] <= instr_in[23:8];
-                            bram_valid_in[3] <= 1;
-                        end
-                        4'b11: begin
-                            bram_temp_in[LINE_WIDTH - 1 - 3 * WORD_WIDTH : LINE_WIDTH - 4 * WORD_WIDTH] <= instr_in[23:8];
-                            bram_valid_in[2] <= 1;
-                        end
-                        4'b100: begin
-                            bram_temp_in[LINE_WIDTH - 1 - 4 * WORD_WIDTH : LINE_WIDTH - 5 * WORD_WIDTH] <= instr_in[23:8];
-                            bram_valid_in[1] <= 1;
-                        end
-                        4'b101: begin
-                            bram_temp_in[LINE_WIDTH - 1 - 5 * WORD_WIDTH : 0] <= instr_in[23:8];
-                            bram_valid_in[0] <= 1;
-                        end
-                        default: load_imm_error <= 1;
-                    endcase
-                    if (bram_valid_in == '1) begin
-                        bram_in[LINE_WIDTH - 1 : 0] <= bram_temp_in[LINE_WIDTH - 1 : 0];
-                        bram_read <= 1;
-                        bram_write <= 0;
-                        bram_valid_in <= 6'b0;
-                    end
-                    else begin
+        end else begin
+            if (instr_valid_in) begin
+                case (instr_in[0:3])
+
+                    // Set Memory Address (sets addr)
+                    OP_SMA: begin 
+                        addr <= instr_in[8:23];
+                        idle_out <= 0;  // WHY NOT RETURN IDLE_OUT <= 1?
                         bram_read <= 0;
                         bram_write <= 0;
                     end
-                    idle_out <= 0;
-                end
-                4'b1010: begin
-                    // We assume that the buffer will keep the valid flag high until memory reads the content and go idle
-                    bram_in[LINE_WIDTH - 1 : 0] <= buffer_read_in[LINE_WIDTH - 1 : 0];
-                    idle_out <= 0;
-                    bram_read <= 1;
-                    bram_write <= 0;
-                end
-                4'b1100: begin
-                    write_flag <= 1;
-                    idle_out <= 0;
-                    bram_read <= 0;
-                    bram_write <= 0;
-                end
+
+                    // Load immediate at address
+                    OP_LOADI: begin
+                        idle_out <= 0;
+
+                        // instr[4:7] contain the bits for which word to set
+                        if (instr[4:7] < 6) begin
+                            bram_valid_in[instr[4:7]] <= 1;
+                            bram_temp_in[LINE_WIDTH - instr[4:7] * WORD_WIDTH - 1 : LINE_WIDTH - (instr[4:7] + 1'b1) * WORD_WIDTH] <= instr_in[8:23];
+                        end else begin
+                            load_imm_error <= 1;
+                        end
+
+                        // If the entire BRAM is valid, set it as ready to write.
+                        if (bram_valid_in == '1) begin
+                            bram_in[LINE_WIDTH - 1 : 0] <= bram_temp_in[LINE_WIDTH - 1 : 0];
+                            bram_read <= 1;
+                            bram_write <= 0;
+                            bram_valid_in <= 6'b0;
+                        end else begin
+                            bram_read <= 0;
+                            bram_write <= 0;
+                        end
+                    end
+                    4'b1010: begin
+                        // We assume that the buffer will keep the valid flag high until memory reads the content and go idle
+                        bram_in[LINE_WIDTH - 1 : 0] <= buffer_read_in[LINE_WIDTH - 1 : 0];
+                        idle_out <= 0;
+                        bram_read <= 1;
+                        bram_write <= 0;
+                    end
+                    4'b1100: begin
+                        write_flag <= 1;
+                        idle_out <= 0;
+                        bram_read <= 0;
+                        bram_write <= 0;
+                    end
                 default: op_code_error <= 1;
             endcase
         end
-        else if (write_flag) begin
+
+        // Hanfei: factor out these if statements to be cleaner
+        if (write_flag) begin
             cycle_ctr <= cycle_ctr + 1;
             if (cycle_ctr == 2'b10) begin
                 abc_out[LINE_WIDTH - 1 : 0] <= bram_out[LINE_WIDTH - 1 : 0];
@@ -138,13 +150,11 @@ module memory #(
                 write_flag <= 0;
                 bram_read <= 0;
                 bram_write <= 1;
-            end
-            else begin
+            end else begin
                 bram_read <= 0;
                 bram_write <= 0;
             end
-        end
-        else begin
+        end else begin
             cycle_ctr <= cycle_ctr + 1;
             if (cycle_ctr == 2'b10) begin
                 addr <= 0;
@@ -161,8 +171,7 @@ module memory #(
                 write_flag <= 0;
                 bram_read <= 0;
                 bram_write <= 0;
-            end
-            else begin
+            end else begin
                 bram_read <= 0;
                 bram_write <= 0;
             end
