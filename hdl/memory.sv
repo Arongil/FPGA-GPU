27f771157b5d8cbbd3cc452e2640d1daeae56071
hbts1 <hfcui@mit.edu>
2023-11-20 22:43:47 -0500

    testbenched memory module

--- hdl/memory.sv
+++ hdl/memory.sv
@@ -21,9 +21,10 @@ module memory #(
     input wire instr_valid_in,
     output logic idle_out, // 1 when idle, 0 when busy
     output logic [LINE_WIDTH - 1 : 0] abc_out,  // for each FMA i, abc_in[i] is laid out as "a b c" 
-    output logic abc_valid_out,
+    output logic abc_valid_out
 );
-
+    // OP_CODEs: read in from FMA_write_buffer 4'b1010
+    // OP_CODEs: write to FMA_read_buffer 4'b1100
 
     enum logic[3:0] {
         // ------------------------------------------------------------------------
@@ -37,13 +38,13 @@ module memory #(
                                //    Places sum of b_reg and val in a_reg
         OP_BGE     = 4'b0100,  // bge(a_reg, b_reg):
                                //    Sets compare_reg to 1 iff a_reg >= b_reg
-        OP_JUMP    = 4'b0101   // jump(jump_to):
+        OP_JUMP    = 4'b0101,  // jump(jump_to):
                                //    Jumps to instruction at immediate index jump_to, if compare_reg is 1.
-        OP_SMA     = 4'b0110   // sma(val):
+        OP_SMA     = 4'b0110,  // sma(val):
                                //    Set memory address to the immediate val in the data cache.
-        OP_LOADI   = 4'b0111   // loadi(a_reg, val):
-                               //    Load immediate val into line at memory address, at word a_reg (not value at a_reg, but the direct bits).
-        OP_LOADB   = 4'b1000   // loadb(val):
+        OP_LOADI   = 4'b0111,  // loadi(reg_a, val):
+                               //    Load immediate val into line at memory address, at word reg_a (not value at a_reg, but the direct bits).
+        OP_LOADB   = 4'b1000,  // loadb(val):
                                //    Load FMA buffer contents into the immediate addr in the data cache.
         OP_WRITEB  = 4'b1001   // writeb(val):
                                //    Write contents of immediate addr in the data cache to FMA blocks. 
@@ -62,6 +63,8 @@ module memory #(
     logic write_flag; // indicates a write operation from the BRAM to buffer
     logic bram_read;
     logic bram_write;
+    logic reset_bram_read; // reset logics after putting one line into BRAM from system
+    logic pull_abc_valid_out_low_flag; // will write a state machine later to make this less painful
 
     always_ff @(posedge clk_in) begin
         if (rst_in) begin
@@ -70,7 +73,6 @@ module memory #(
             bram_temp_in <= 0;
             bram_in <= 0;
             bram_valid_in <= 0;
-            bram_out <= 0;
             abc_out <= 0;
             abc_valid_out <= 0;
             load_imm_error <= 0;
@@ -79,6 +81,8 @@ module memory #(
             write_flag <= 0;
             bram_read <= 0;
             bram_write <= 0;
+            reset_bram_read <= 0;
+            pull_abc_valid_out_low_flag <= 0;
         end else begin
             if (instr_valid_in) begin
                 case (instr_in[0:3])
@@ -86,7 +90,7 @@ module memory #(
                     // Set Memory Address (sets addr)
                     OP_SMA: begin 
                         addr <= instr_in[8:23];
-                        idle_out <= 0;  // WHY NOT RETURN IDLE_OUT <= 1?
+                        idle_out <= 1;  // WHY NOT RETURN IDLE_OUT <= 1? Resolved
                         bram_read <= 0;
                         bram_write <= 0;
                     end
@@ -95,24 +99,32 @@ module memory #(
                     OP_LOADI: begin
                         idle_out <= 0;
 
-                        // instr[4:7] contain the bits for which word to set
-                        if (instr[4:7] < 6) begin
-                            bram_valid_in[instr[4:7]] <= 1;
-                            bram_temp_in[LINE_WIDTH - instr[4:7] * WORD_WIDTH - 1 : LINE_WIDTH - (instr[4:7] + 1'b1) * WORD_WIDTH] <= instr_in[8:23];
+                        // instr_in[4:7] contain the bits for which word to set
+                        if (instr_in[4:7] < 6) begin
+                            bram_valid_in[instr_in[4:7]] <= 1;
+                            bram_temp_in[LINE_WIDTH - (instr_in[4:7] + 1'b1) * WORD_WIDTH +: WORD_WIDTH] <= instr_in[8:23];
+                            // bram_temp_in[LINE_WIDTH - instr_in[4:7] * WORD_WIDTH - 1 : LINE_WIDTH - (instr_in[4:7] + 1'b1) * WORD_WIDTH] <= instr_in[8:23];
+                            // (x+y-1) = LINE_WIDTH - instr_in[4:7] * WORD_WIDTH - 1
+                            // (x) = LINE_WIDTH - (instr_in[4:7] + 1'b1) * WORD_WIDTH
+                            // (y-1) = LINE_WIDTH - instr_in[4:7] * WORD_WIDTH - 1 - LINE_WIDTH + (instr_in[4:7] + 1'b1) * WORD_WIDTH
+                            // = - (instr_in[4:7] * WORD_WIDTH + 1) + (instr_in[4:7] + 1'b1) * WORD_WIDTH
+                            // = - (instr_in[4:7] * WORD_WIDTH + 1) + (instr_in[4:7] * WORD_WIDTH) + WORD_WIDTH
+                            // = WORD_WIDTH - 1
+                            // y = WORD_WIDTH
+                            // w[x  +: y] == w[(x+y-1) : x]
                         end else begin
                             load_imm_error <= 1;
                         end
-
-                        // If the entire BRAM is valid, set it as ready to write.
-                        if (bram_valid_in == '1) begin
-                            bram_in[LINE_WIDTH - 1 : 0] <= bram_temp_in[LINE_WIDTH - 1 : 0];
-                            bram_read <= 1;
-                            bram_write <= 0;
-                            bram_valid_in <= 6'b0;
-                        end else begin
-                            bram_read <= 0;
-                            bram_write <= 0;
-                        end
+                        bram_read <= 0;
+                        bram_write <= 0;
+                    end
+                    4'b1110: begin
+                        // NEW INSTRUCTION: aftering putting 6 words on the line, send a new instruction to flush line into BRAM
+                        bram_in[LINE_WIDTH - 1 : 0] <= bram_temp_in[LINE_WIDTH - 1 : 0];
+                        bram_read <= 1;
+                        bram_write <= 0;
+                        bram_valid_in <= 6'b0;
+                        reset_bram_read <= 1;
                     end
                     4'b1010: begin
                         // We assume that the buffer will keep the valid flag high until memory reads the content and go idle
@@ -120,78 +132,82 @@ module memory #(
                         idle_out <= 0;
                         bram_read <= 1;
                         bram_write <= 0;
+                        reset_bram_read <= 1;
                     end
                     4'b1100: begin
                         write_flag <= 1;
                         idle_out <= 0;
                         bram_read <= 0;
-                        bram_write <= 0;
+                        bram_write <= 1;
                     end
-                default: op_code_error <= 1;
-            endcase
-        end
-
-        // Hanfei: factor out these if statements to be cleaner
-        if (write_flag) begin
-            cycle_ctr <= cycle_ctr + 1;
-            if (cycle_ctr == 2'b10) begin
-                abc_out[LINE_WIDTH - 1 : 0] <= bram_out[LINE_WIDTH - 1 : 0];
-                addr <= 0;
-                bram_temp_in <= 0;
-                bram_in <= 0;
-                bram_valid_in <= 0;
-                bram_out <= 0;
-                idle_out <= 1;
-                abc_out <= 0;
+                    default: op_code_error <= 1;
+                endcase
                 abc_valid_out <= 0;
-                load_imm_error <= 0;
-                op_code_error <= 0;
-                cycle_ctr <= 0;
-                write_flag <= 0;
-                bram_read <= 0;
-                bram_write <= 1;
-            end else begin
-                bram_read <= 0;
-                bram_write <= 0;
             end
-        end else begin
-            cycle_ctr <= cycle_ctr + 1;
-            if (cycle_ctr == 2'b10) begin
-                addr <= 0;
+
+            // Hanfei: factor out these if statements to be cleaner
+            else if (write_flag) begin
+                if (cycle_ctr == 2'b01) begin
+                    abc_out <= bram_out;
+                    abc_valid_out <= 1;
+                    bram_temp_in <= 0;
+                    bram_in <= 0;
+                    bram_valid_in <= 0;
+                    idle_out <= 1;
+                    load_imm_error <= 0;
+                    op_code_error <= 0;
+                    cycle_ctr <= 0;
+                    write_flag <= 0;
+                    bram_read <= 0;
+                    bram_write <= 0;
+                    pull_abc_valid_out_low_flag <= 1;
+                end else begin
+                    cycle_ctr <= cycle_ctr + 1;
+                    bram_read <= 0;
+                    bram_write <= 0;
+                end
+            end 
+            else if (reset_bram_read) begin
                 bram_temp_in <= 0;
                 bram_in <= 0;
                 bram_valid_in <= 0;
-                bram_out <= 0;
                 idle_out <= 1;
-                abc_out <= 0;
-                abc_valid_out <= 0;
                 load_imm_error <= 0;
                 op_code_error <= 0;
-                cycle_ctr <= 0;
-                write_flag <= 0;
                 bram_read <= 0;
-                bram_write <= 0;
-            end else begin
-                bram_read <= 0;
-                bram_write <= 0;
+                reset_bram_read <= 0;
+                abc_valid_out <= 0;
+            end
+            else if (pull_abc_valid_out_low_flag) begin
+                abc_valid_out <= 0;
+                pull_abc_valid_out_low_flag <= 0;
             end
         end
     end
 
-    xilinx_single_port_ram_read_first #(
-        .RAM_WIDTH(LINE_WIDTH),        // Specify RAM data width
-        .RAM_DEPTH(WORD_WIDTH),        // Specify RAM depth
-        .RAM_PERFORMANCE("HIGH_PERFORMANCE"), // Select "HIGH_PERFORMANCE" or "LOW_LATENCY" 
-        .INIT_FILE())          // Specify name/location of RAM initialization file if using one (leave blank if not)
-    ) data_cache_BRAM (
-        .addra(addr),     // Address bus, width is ADDR_LENGTH
-        .dina(bram_in),       // RAM input data, width is WIDTH
-        .clka(clk_in),       // Clock
-        .wea(bram_read),         // Write enable
-        .ena(1),         // RAM Enable, for additional power savings, disable port when not in use
-        .rsta(0),       // Output reset (does not affect memory contents)
-        .regcea(bram_write),    // Output register enable
-        .douta(bram_out)      // RAM output data, width is WIDTH
+    xilinx_true_dual_port_read_first_2_clock_ram #(
+        .RAM_WIDTH(LINE_WIDTH),                       // Specify RAM data width
+        .RAM_DEPTH(WORD_WIDTH),                     // Specify RAM depth (number of entries)
+        .RAM_PERFORMANCE("HIGH_PERFORMANCE"), // Select "HIGH_PERFORMANCE" or "LOW_LATENCY"
+        .INIT_FILE("")                        // Specify name/location of RAM initialization file if using one (leave blank if not)
+    ) memory_BRAM (
+        .addra(addr),   // Port A address bus, width determined from RAM_DEPTH
+        .addrb(),   // Port B address bus, width determined from RAM_DEPTH
+        .dina(bram_in),     // Port A RAM input data, width determined from RAM_WIDTH
+        .dinb(),     // Port B RAM input data, width determined from RAM_WIDTH
+        .clka(clk_in),     // Port A clock
+        .clkb(),     // Port B clock
+        .wea(bram_read),       // Port A write enable
+        .web(),       // Port B write enable
+        .ena(1),       // Port A RAM Enable, for additional power savings, disable port when not in use
+        .enb(),       // Port B RAM Enable, for additional power savings, disable port when not in use
+        .rsta(0),     // Port A output reset (does not affect memory contents)
+        .rstb(),     // Port B output reset (does not affect memory contents)
+        .regcea(bram_write), // Port A output register enable
+        .regceb(), // Port B output register enable
+        .douta(bram_out),   // Port A RAM output data, width determined from RAM_WIDTH
+        .doutb()    // Port B RAM output data, width determined from RAM_WIDTH
+    );
 endmodule
 
 `default_nettype wire
---
+++ sim/memory_tb.sv
@@ -0,0 +1,270 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+module memory_tb;
+
+  parameter FMA_COUNT = 2;  // number of FMAs to prepare data for in a simultaneous read
+  parameter WORD_WIDTH = 16;  // number of bits per number aka width of a word
+  parameter LINE_WIDTH = 96;  // width of a line, FMA_COUNT * 3 * WORD_WIDTH = 2 * 3 * 16 = 96
+  parameter ADDR_LENGTH = $clog2(375);  // 96 bits in a line. 36kb/96 = 375
+  parameter INSTRUCTION_WIDTH = 32;  // number of bits per instruction
+
+  //make logics for inputs and outputs!
+  logic clk_in;
+  logic rst_in;
+  logic [LINE_WIDTH - 1 : 0] buffer_read_in;
+  logic [INSTRUCTION_WIDTH - 1 : 0] instr_in;
+  logic instr_valid_in;
+  logic [LINE_WIDTH - 1 : 0] abc_out;
+  logic abc_valid_out;
+
+  memory #(.FMA_COUNT(FMA_COUNT), 
+           .WORD_WIDTH(WORD_WIDTH), 
+           .LINE_WIDTH(LINE_WIDTH), 
+           .ADDR_LENGTH(ADDR_LENGTH), 
+           .INSTRUCTION_WIDTH(INSTRUCTION_WIDTH)) uut  // uut = unit under test
+          (.clk_in(clk_in),
+           .rst_in(rst_in),
+           .buffer_read_in(buffer_read_in),
+           .instr_in(instr_in),
+           .instr_valid_in(instr_valid_in),
+           .abc_out(abc_out),
+           .abc_valid_out(abc_valid_out));
+
+  always begin
+    #5;  //every 5 ns switch...so period of clock is 10 ns...100 MHz clock
+    clk_in = !clk_in;
+  end
+
+  //initial block...this is our test simulation
+  initial begin
+    $dumpfile("memory.vcd"); //file to store value change dump (vcd)
+    $dumpvars(0, memory_tb); //store everything at the current level and below
+    $display("Starting Sim\n"); //print nice message
+    clk_in = 0; //initialize clk (super important)
+    rst_in = 0; //initialize rst (super important)
+    #5  //wait a little bit of time at beginning
+    rst_in = 1; //reset system
+    #10; //hold high for a few clock cycles
+    rst_in = 0;
+    #10;
+
+    // We'll perform the following test cases:
+    //   (pre_lim, can't check directly) Give an instruction that gives an address
+    //   1. Give instructions that fill the line with that address
+    //   2. Give an instruction that asks the memory to take the line at this address and fill the fma_read_buffer with it
+    //   3. Give an instruction that asks the memory to take a line from the fma_write_buffer and fill in the line at the address with it
+    //
+    
+    // OP codes: 
+    // enum logic[3:0] {
+    //     // ------------------------------------------------------------------------
+    //     // | 4 bit op code | 4 bit reg | 16 bit immediate | 4 bit reg | 4 bit reg |
+    //     // ------------------------------------------------------------------------
+    //     OP_NOP     = 4'b0000,  // no op
+    //     OP_END     = 4'b0001,  // end execution 
+    //     OP_XOR     = 4'b0010,  // xor(a_reg, b_reg):
+    //                            //    Places bitwise xor in a_reg
+    //     OP_ADDI    = 4'b0011,  // addi(a_reg, b_reg, val):
+    //                            //    Places sum of b_reg and val in a_reg
+    //     OP_BGE     = 4'b0100,  // bge(a_reg, b_reg):
+    //                            //    Sets compare_reg to 1 iff a_reg >= b_reg
+    //     OP_JUMP    = 4'b0101,  // jump(jump_to):
+    //                            //    Jumps to instruction at immediate index jump_to, if compare_reg is 1.
+    //     OP_SMA     = 4'b0110,  // sma(val):
+    //                            //    Set memory address to the immediate val in the data cache.
+    //     OP_LOADI   = 4'b0111,  // loadi(reg_a, val):
+    //                            //    Load immediate val into line at memory address, at word reg_a (not value at a_reg, but the direct bits).
+    //     OP_LOADB   = 4'b1000,  // loadb(val):
+    //                            //    Load FMA buffer contents into the immediate addr in the data cache.
+    //     OP_WRITEB  = 4'b1001   // writeb(val):
+    //                            //    Write contents of immediate addr in the data cache to FMA blocks. 
+    // } isa;
+    // Use first 4-bit reg for loading immediate. 4'b0 means loading 0th word in the line, 4'b1 means 1st, ... , 4'b101 means 5th 
+    // We have FMA_COUNT * 3 = 6 words per line right now
+    // ADDR_LENGTH needs only 9 bits for now (since we are reading entire lines), which lives in the immediate section of the instr
+
+    /*
+    NOTE: ADDRESS IS ONLY 9 BITS OUT OF THE 16 IMMEDIATE BITS
+    */
+
+    // Prelim -- give an address to the BRAM
+    instr_in = 32'b0110___0000___0000_0001_0111_1000___0000___0000;
+    instr_valid_in = 1;
+    #10;
+    instr_in = 32'b0;
+    instr_valid_in = 0;
+
+    #10;
+
+    // TEST CASE #1
+    // Give instructions that fill the line with that address
+    instr_in = 32'b0111___0000___1000_1000_1000_1000___0000___0000;
+    instr_valid_in = 1;
+    #10;
+    instr_in = 32'b0;
+    instr_valid_in = 0;
+    #10;
+    instr_in = 32'b0111___0001___1000_1000_1000_0111___0000___0000;
+    instr_valid_in = 1;
+    #10;
+    instr_in = 32'b0;
+    instr_valid_in = 0;
+    #10;
+    instr_in = 32'b0111___0010___1000_1000_1000_0110___0000___0000;
+    instr_valid_in = 1;
+    #10;
+    instr_in = 32'b0;
+    instr_valid_in = 0;
+    #10;
+    instr_in = 32'b0111___0011___1000_1000_1000_0101___0000___0000;
+    instr_valid_in = 1;
+    #10;
+    instr_in = 32'b0;
+    instr_valid_in = 0;
+    #10;
+    instr_in = 32'b0111___0100___1000_1000_1000_0100___0000___0000;
+    instr_valid_in = 1;
+    #10;
+    instr_in = 32'b0;
+    instr_valid_in = 0;
+    #10;
+    instr_in = 32'b0111___0101___1000_1000_1000_0011___0000___0000;
+    instr_valid_in = 1;
+    #10;
+    instr_in = 32'b0;
+    instr_valid_in = 0;
+    #10;
+    instr_in = 32'b1110___0110___1000_1000_1000_0011___0000___0000;
+    instr_valid_in = 1;
+    #10;
+    instr_in = 32'b0;
+    instr_valid_in = 0;
+
+    // Give the BRAMs 2 cycles to take in the data
+    #20;
+
+    // TEST CASE #2
+    // Give an instruction that asks the memory to take the line at this address and fill the fma_read_buffer with it
+    instr_in = 32'b1100___0000___1000_1000_1000_1000___0000___0000;
+    instr_valid_in = 1;
+    #10;
+    instr_in = 32'b0;
+    instr_valid_in = 0;
+    #20; // 2 cycles to get stuff out of the bram, 1 cycle to put stuff into buffer
+
+    // TEST CASE #3
+    // Give an instruction that asks the memory to take a line from the fma_write_buffer and fill in the line at the address with it
+    instr_in = 32'b1010___0000___1000_1000_1000_1000___0000___0000;
+    buffer_read_in = 96'hA000_A000_A000_A000_A000_A000;
+    instr_valid_in = 1;
+    #10;
+    instr_in = 32'b0;
+    instr_valid_in = 0;
+    #10;
+
+    // Now take that line out and make sure it is what we put in
+    #10;
+    instr_in = 32'b1100___0000___1000_1000_1000_1000___0000___0000;
+    instr_valid_in = 1;
+    #10;
+    instr_in = 32'b0;
+    instr_valid_in = 0;
+    #20; // 2 cycles to get stuff out of the bram, 1 cycle to put stuff into buffer
+
+//
+//
+//
+
+    // Prelim -- give an address to the BRAM
+    instr_in = 32'b0110___0000___0000_0101_0111_1000___0000___0000;
+    instr_valid_in = 1;
+    #10;
+    instr_in = 32'b0;
+    instr_valid_in = 0;
+
+    #10;
+
+    // TEST CASE #1
+    // Give instructions that fill the line with that address
+    instr_in = 32'b0111___0000___1100_1000_1000_1000___0000___0000;
+    instr_valid_in = 1;
+    #10;
+    instr_in = 32'b0;
+    instr_valid_in = 0;
+    #10;
+    instr_in = 32'b0111___0001___1100_1000_1000_0111___0000___0000;
+    instr_valid_in = 1;
+    #10;
+    instr_in = 32'b0;
+    instr_valid_in = 0;
+    #10;
+    instr_in = 32'b0111___0010___1100_1000_1000_0110___0000___0000;
+    instr_valid_in = 1;
+    #10;
+    instr_in = 32'b0;
+    instr_valid_in = 0;
+    #10;
+    instr_in = 32'b0111___0011___1100_1000_1000_0101___0000___0000;
+    instr_valid_in = 1;
+    #10;
+    instr_in = 32'b0;
+    instr_valid_in = 0;
+    #10;
+    instr_in = 32'b0111___0100___1100_1000_1000_0100___0000___0000;
+    instr_valid_in = 1;
+    #10;
+    instr_in = 32'b0;
+    instr_valid_in = 0;
+    #10;
+    instr_in = 32'b0111___0101___1100_1000_1000_0011___0000___0000;
+    instr_valid_in = 1;
+    #10;
+    instr_in = 32'b0;
+    instr_valid_in = 0;
+    #10;
+    instr_in = 32'b1110___0110___1100_1000_1000_0011___0000___0000;
+    instr_valid_in = 1;
+    #10;
+    instr_in = 32'b0;
+    instr_valid_in = 0;
+
+    // Give the BRAMs 2 cycles to take in the data
+    #20;
+
+    // TEST CASE #2
+    // Give an instruction that asks the memory to take the line at this address and fill the fma_read_buffer with it
+    instr_in = 32'b1100___0000___1000_1000_1000_1000___0000___0000;
+    instr_valid_in = 1;
+    #10;
+    instr_in = 32'b0;
+    instr_valid_in = 0;
+    #20; // 2 cycles to get stuff out of the bram, 1 cycle to put stuff into buffer
+
+    // TEST CASE #3
+    // Give an instruction that asks the memory to take a line from the fma_write_buffer and fill in the line at the address with it
+    instr_in = 32'b1010___0000___1000_1000_1000_1000___0000___0000;
+    buffer_read_in = 96'hAA00_A000_A000_A000_A000_A000;
+    instr_valid_in = 1;
+    #10;
+    instr_in = 32'b0;
+    instr_valid_in = 0;
+    #10;
+
+    // Now take that line out and make sure it is what we put in
+    #10;
+    instr_in = 32'b1100___0000___1000_1000_1000_1000___0000___0000;
+    instr_valid_in = 1;
+    #10;
+    instr_in = 32'b0;
+    instr_valid_in = 0;
+    #20; // 2 cycles to get stuff out of the bram, 1 cycle to put stuff into buffer
+
+    #100;
+    $finish;
+
+  end
+
+endmodule // fma_memory_buffer_tb
+
+`default_nettype wire
